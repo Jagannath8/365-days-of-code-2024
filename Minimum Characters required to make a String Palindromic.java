8644fa8d7f47c203adb8d2e4cc2a80792815038e
Jagannath Pal <64650848+Jagannath8@users.noreply.github.com>
2024-02-20 22:04:13 +0530

    Day 50

---
+++ Minimum Characters required to make a String Palindromic.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int solve(String A) {
+        int n=A.length();
+        int l=n/2, r=n/2;
+        if(n%2==0)
+            l--;
+        while(l>=0){
+            if(A.charAt(r)==A.charAt(l)) {
+                l--;
+                r++;
+            }
+            else 
+                r--;
+        }
+        return (n-r);
+    }
+}
